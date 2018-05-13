92b0c00cc37ebfe72ecd9575aa7a3eea94ea428a
Mirko Bristle <mirko.bristle@gmail.com>
2018-05-13 23:11:29 +0200

    overview of summary SVM

---
+++ Analysis/Modeling/functions/get_svm.py
@@ -0,0 +1,99 @@
+# Several functions to train specific SVM models
+# return parameters may vary depending on the need of further processing
+
+
+
+def first_view_SVM(X: float, y: int, test_train_ratio: float, random_state: int, X_t: float = None, y_t: int = None):
+    "Function splitting the DS, calc linear SVM, predict data, calc accuracy and confusion matrix"
+
+    # importing necessary libraries
+    from sklearn.svm import SVC
+    from sklearn.metrics import confusion_matrix
+
+    # dividing X, y into train and test data
+    X_train, X_test, y_train, y_test = get_train_test(
+        X, y, test_train_ratio, random_state, X_t, y_t)
+
+    # training a linear SVM classifier
+    svm_model_linear = SVC(kernel='linear', C=1).fit(X_train, y_train)
+    svm_predictions = svm_model_linear.predict(X_test)
+
+    # creating a confusion matrix
+    cm = confusion_matrix(y_test, svm_predictions)
+
+    # model accuracy for X_test
+    accuracy = svm_model_linear.score(X_test, y_test)
+
+    return accuracy, cm
+
+
+def each_class_CV(X: float, y: int, test_train_ratio: float, random_state: int, X_t: float = None, y_t: int = None,n_jobs:int=4):
+    "get SVM for the class specified"
+
+    from sklearn import svm, datasets
+    from sklearn.metrics import roc_curve, auc
+    from sklearn.model_selection import train_test_split
+    from sklearn.preprocessing import label_binarize
+    from sklearn.multiclass import OneVsRestClassifier
+    from scipy import interp
+
+    y, n_classes = binarize(y)
+    y_t = binarize(y_t)
+
+    # shuffle and split training and test sets
+    X_train, X_test, y_train, y_test = get_train_test(
+        X, y, test_train_ratio, random_state, X_t, y_t)
+
+    # Learn to predict each class against the other
+    classifier = OneVsRestClassifier(svm.SVC(kernel='linear', probability=True,
+                                             random_state=0),n_jobs)
+    y_score = classifier.fit(X_train, y_train).decision_function(X_test)
+
+    return classifier, y_score, y_test, n_classes
+
+
+def permutationTesting(X: float, y: int,group=None, cv=None,n_permutations: int = 100, random_state: int = 0,n_jobs:int=4):
+    "get SVM for the class specified"
+    from sklearn import svm, datasets
+    from sklearn.metrics import confusion_matrix
+    from sklearn.model_selection import train_test_split
+    from sklearn.metrics import roc_auc_score
+    from sklearn.model_selection import StratifiedKFold
+    from sklearn.model_selection import permutation_test_score
+
+    random_state = 0
+    classifier = svm.SVC(kernel='linear', random_state=random_state)
+    if cv is None:
+        cv = StratifiedKFold(n_splits=3)
+
+    score, permutation_scores, pvalue = permutation_test_score(
+        classifier, X, y,groups=group, scoring="accuracy", cv=cv, n_permutations=n_permutations, n_jobs=n_jobs)
+
+    return score, permutation_scores, pvalue
+
+
+
+def get_train_test(X: float, y: int, test_train_ratio: float, random_state: int, X_t: float = None, y_t: int = None):
+    "split data to train and testset depending on input"
+    from sklearn.model_selection import train_test_split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_train_ratio, random_state=random_state)
+
+    if X_t is not None and y_t is not None:
+        X_test = X_t
+        y_test = y_t
+    return X_train, X_test, y_train, y_test
+
+
+def binarize(y):
+    if y is None:
+        return None
+
+    from sklearn import preprocessing
+    lb = preprocessing.LabelBinarizer()
+    lb.fit(y)
+    # print(lb.classes_.shape[0])
+    n_classes = lb.classes_.shape[0]
+    y = lb.transform(y)
+    return y, n_classes
+
+
---
+++ Analysis/Modeling/functions/plot.py
@@ -0,0 +1,162 @@
+# Plot the different SVM
+
+
+def each_class_CV(classifier, y_score, y_test, n_classes):
+    ""
+    from sklearn.metrics import roc_curve, auc
+    import numpy as np
+    from scipy import interp
+    import matplotlib.pyplot as plt
+    from itertools import cycle
+
+    # CONSTANTS
+    lw = 2
+
+    fpr, tpr, roc_auc = each_class_ROC(n_classes, y_test, y_score)
+
+    # Compute micro-average ROC curve and ROC area
+    fpr["micro"], tpr["micro"], _ = roc_curve(y_test.ravel(), y_score.ravel())
+    roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+
+    # Compute macro-average ROC curve and ROC area
+
+    # First aggregate all false positive rates
+    all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
+
+    # Then interpolate all ROC curves at this points
+    mean_tpr = np.zeros_like(all_fpr)
+    for i in range(n_classes):
+        mean_tpr += interp(all_fpr, fpr[i], tpr[i])
+
+    # Finally average it and compute AUC
+    mean_tpr /= n_classes
+
+    fpr["macro"] = all_fpr
+    tpr["macro"] = mean_tpr
+    roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
+
+    # Plot all ROC curves
+    plt.figure()
+    plt.plot(fpr["micro"], tpr["micro"],
+             label='micro-average ROC curve (area = {0:0.2f})'
+                   ''.format(roc_auc["micro"]),
+             color='deeppink', linestyle=':', linewidth=4)
+
+    plt.plot(fpr["macro"], tpr["macro"],
+             label='macro-average ROC curve (area = {0:0.2f})'
+                   ''.format(roc_auc["macro"]),
+             color='navy', linestyle=':', linewidth=4)
+
+    colors = cycle(['aqua', 'darkorange', 'cornflowerblue'])
+    for i, color in zip(range(n_classes), colors):
+        plt.plot(fpr[i], tpr[i], color=color, lw=lw,
+                 label='ROC curve of class {0} (area = {1:0.2f})'
+                       ''.format(i + 1, roc_auc[i]))
+
+    plt.plot([0, 1], [0, 1], 'k--', lw=lw)
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Some extension of Receiver operating characteristic to multi-class')
+    plt.legend(loc="lower right")
+    plt.show()
+    return
+
+
+def each_class_ROC(n_classes, y_test, y_score):
+    "Compute ROC curve and ROC area for each class"
+    from sklearn.metrics import roc_curve, auc
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
+
+    for i in range(n_classes):
+        fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_score[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+
+    return fpr, tpr, roc_auc
+
+
+def stratifiedCV(X, y, classifier):
+    # Classification and ROC analysis
+    from sklearn.metrics import roc_curve, auc
+    from sklearn.model_selection import StratifiedKFold
+    from scipy import interp
+    import matplotlib.pyplot as plt
+    import numpy as np
+    import functions.get_svm as get_SVM
+
+    y_b,n_classes = get_SVM.binarize(y)
+
+    for k in range(n_classes):
+        y = y_b[:,k]
+        cv = StratifiedKFold(n_splits=6)
+        tprs = []
+        aucs = []
+        mean_fpr = np.linspace(0, 1, 100)
+
+        i = 0
+        for train, test in cv.split(X, y):
+            probas_ = classifier.fit(X[train], y[train]).predict_proba(X[test])
+            # Compute ROC curve and area the curve
+            fpr, tpr, thresholds = roc_curve(y[test], probas_[:, 1])
+
+
+            tprs.append(interp(mean_fpr, fpr, tpr))
+            tprs[-1][0] = 0.0
+            roc_auc = auc(fpr, tpr)
+            aucs.append(roc_auc)
+            plt.plot(fpr, tpr, lw=1, alpha=0.3,
+                     label='ROC fold %d (AUC = %0.2f)' % (i, roc_auc))
+
+            i += 1
+
+        plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r',
+                 label='Luck', alpha=.8)
+
+        mean_tpr = np.mean(tprs, axis=0)
+        mean_tpr[-1] = 1.0
+        mean_auc = auc(mean_fpr, mean_tpr)
+        std_auc = np.std(aucs)
+        plt.plot(mean_fpr, mean_tpr, color='b',
+                 label=r'Mean ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, std_auc),
+                 lw=2, alpha=.8)
+
+        std_tpr = np.std(tprs, axis=0)
+        tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
+        tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
+        plt.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2,
+                         label=r'$\pm$ 1 std. dev.')
+
+        plt.xlim([-0.05, 1.05])
+        plt.ylim([-0.05, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('Receiver operating characteristic of class %d'% (k))
+        plt.legend(loc="lower right")
+        plt.show()
+    return
+
+def permutation(permutation_scores,score,pvalue,n_classes):
+    import matplotlib.pyplot as plt
+    # View histogram of permutation scores
+    plt.hist(permutation_scores, 20, label='Permutation scores',
+             edgecolor='black')
+    ylim = plt.ylim()
+    # BUG: vlines(..., linestyle='--') fails on older versions of matplotlib
+    # plt.vlines(score, ylim[0], ylim[1], linestyle='--',
+    #          color='g', linewidth=3, label='Classification Score'
+    #          ' (pvalue %s)' % pvalue)
+    # plt.vlines(1.0 / n_classes, ylim[0], ylim[1], linestyle='--',
+    #          color='k', linewidth=3, label='Luck')
+    plt.plot(2 * [score], ylim, '--g', linewidth=3,
+             label='Classification Score'
+                   ' (pvalue %s)' % pvalue)
+    plt.plot(2 * [1. / n_classes], ylim, '--k', linewidth=3, label='Luck')
+
+    plt.ylim(ylim)
+    plt.legend()
+    plt.xlabel('Score')
+    plt.show()
+    return
