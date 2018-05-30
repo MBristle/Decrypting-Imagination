2dbdd2ca4429c2ba047cf57cf5504038211ebedb
Mirko Bristle <mirko.bristle@gmail.com>
2018-05-30 02:05:17 +0200

    bug fix with MinMaxScalar

--- Analysis/Modeling/script_kFold_eachClass_AUC.py
+++ Analysis/Modeling/script_kFold_eachClass_AUC.py
@@ -9,18 +9,30 @@ import functions.load_dataset as ld
 from sklearn import svm
 from sklearn.multiclass import OneVsRestClassifier
 import multiprocessing
+from sklearn import preprocessing
+import sys
+
+
+print(sys.argv)
+if len(sys.argv)>1:
+        starting = int(sys.argv[1])
+        ending = starting + 1 if len(sys.argv)<3 else int(sys.argv[2])
+else:
+        raise IOError('not existing argument')
+
 
 # import Data in features X and targets y
 Xs_p, ys_p, Xs_i, ys_i, group_s_p, group_s_i = ld.load_summary()
-Xm_p, ym_p, Xm_i, ym_i, group_m_p, group_m_i = ld.load_map()
+Xm_p, ym_p, Xm_i, ym_i, group_m_p, group_m_i = ld.load_map(split=6)
 
 TEST_TRAIN_RATIO = 0.7
 n_jobs = multiprocessing.cpu_count()
 #print(f'cpu cores: {n_jobs:d}')
 rnd.seed = 0
-RANDOM_STATE = rnd.randint(0, 2 ** 32 - 1)
+RANDOM_STATE = 7320712#rnd.randint(0, 2 ** 32 - 1)
 #print(f'Random state: {RANDOM_STATE:d}')
-
+out= ('each_class_s_r'+str(RANDOM_STATE)+'from'+str(starting)+'to'+str(ending))
+print(out)
 COMPARISON = ["S_IID: p->p", "S_IID: i->i", "S_IID: p->i", "S_IID: i->p",
               "S_Vpn: p->p", "S_Vpn: i->i", "S_Vpn: p->i", "S_Vpn: i->p",
               "F_IID: p->p", "F_IID: i->i", "F_IID: p->i", "F_IID: i->p",
@@ -35,12 +47,12 @@ GROUP = [None, None, None, None,
 skf_cv = StratifiedKFold(n_splits=5, random_state=RANDOM_STATE, shuffle=True)
 gkf_cv = LeaveOneGroupOut()  # GroupKFold(n_splits=5)
 CROSSVAL = [skf_cv, skf_cv, skf_cv, skf_cv, gkf_cv, gkf_cv, gkf_cv, gkf_cv,
-            skf_cv, skf_cv, skf_cv, skf_cv, gkf_cv, gkf_cv, gkf_cv, gkf_cv, ]
+            skf_cv, skf_cv, skf_cv, skf_cv, gkf_cv, gkf_cv, gkf_cv, gkf_cv]
 
-X_train = [Xs_p, Xs_i, Xs_p, Xs_i, Xm_p, Xm_i, Xm_p, Xm_i]
-y_train = [ys_p, ys_i, ys_p, ys_i, ym_p, ym_i, ym_p, ym_i]
-X_test = [Xs_p, Xs_i, Xs_i, Xs_p, Xm_p, Xm_i, Xm_i, Xm_p]
-y_test = [ys_p, ys_i, ys_i, ys_p, ym_p, ym_i, ym_i, ym_p]
+X_train = [Xs_p, Xs_i, Xs_p, Xs_i,Xs_p, Xs_i, Xs_p, Xs_i, Xm_p, Xm_i, Xm_p, Xm_i, Xm_p, Xm_i, Xm_p, Xm_i]
+y_train = [ys_p, ys_i, ys_p, ys_i,ys_p, ys_i, ys_p, ys_i, ym_p, ym_i, ym_p, ym_i, ym_p, ym_i, ym_p, ym_i]
+X_test = [Xs_p, Xs_i, Xs_i, Xs_p, Xs_p, Xs_i, Xs_i, Xs_p, Xm_p, Xm_i, Xm_i, Xm_p, Xm_p, Xm_i, Xm_i, Xm_p]
+y_test = [ys_p, ys_i, ys_i, ys_p, ys_p, ys_i, ys_i, ys_p, ym_p, ym_i, ym_i, ym_p, ym_p, ym_i, ym_i, ym_p]
 
 Xlen = len(X_train)
 
@@ -61,9 +73,9 @@ roc_auc_all = list()
 mean_fpr_all = list()
 
 # Learn to predict each class against the other
-classifier = OneVsRestClassifier(svm.SVC(kernel='linear', probability=True,
+classifier = OneVsRestClassifier(svm.SVC(kernel='linear', C=1.0, probability=True,cache_size=200, max_iter=100000,
                                          random_state=RANDOM_STATE))
-for i in range(int(LEN_COMP/2),LEN_COMP):  #LEN_COMP
+for i in range(starting,ending):  #LEN_COMP
     print("start CV testing: ", COMPARISON[i])
 
     y_train_b, d = get_svm.binarize(y_train[i % Xlen])
@@ -93,19 +105,22 @@ for i in range(int(LEN_COMP/2),LEN_COMP):  #LEN_COMP
         ii = 0
         for train, test in CROSSVAL[i].split(X=X_train[i % Xlen], y=y_train_bk, groups=GROUP[i]):
             print('     it: ', ii)
+            scaler = preprocessing.MinMaxScaler().fit(X_train[i % Xlen][train])
+            c_X_test = scaler.transform(X_test[i % Xlen][test])
+            c_X_train = scaler.transform(X_train[i % Xlen][train])
 
             test_selection_c.append(test)
-            X_tested_c.append(X_test[i % Xlen][test])
+            X_tested_c.append(c_X_test)
             y_tested_c.append(y_test_bk[test])
 
             # train
-            svm_model_tmp = classifier.fit(X_train[i % Xlen][train], y_train_bk[train])
+            svm_model_tmp = classifier.fit(c_X_train, y_train_bk[train]) 
             svm_model_c.append(svm_model_tmp)
 
             # test
-            probas_ = svm_model_tmp.predict_proba(X_test[i % Xlen][test])
+            probas_ = svm_model_tmp.predict_proba(c_X_test)
             probas_c.append(probas_)
-            y_score_tmp = svm_model_tmp.predict(X_test[i % Xlen][test])
+            y_score_tmp = svm_model_tmp.predict(c_X_test)
             y_score_c.append(y_score_tmp)
 
             # Compute ROC curve and area the curve
@@ -116,6 +131,7 @@ for i in range(int(LEN_COMP/2),LEN_COMP):  #LEN_COMP
             tprs.append(interp(mean_fpr, fpr, tpr))
             tprs[-1][0] = 0.0
             roc_auc = auc(fpr, tpr)
+            print(roc_auc)
             aucs.append(roc_auc)
             # plt.plot(fpr, tpr, lw=1, alpha=0.3,
             #         label='ROC fold %d (AUC = %0.2f)' % (ii, roc_auc))
@@ -138,7 +154,7 @@ for i in range(int(LEN_COMP/2),LEN_COMP):  #LEN_COMP
         tpr_all.append(tpr_c)
         mean_fpr_all.append(mean_fpr)
 
-np.savez('each_class_r%d' % RANDOM_STATE,
+np.savez(out,
          comparison=COMPARISON[i],
          class_tested=np.asarray(class_tested),
          svm_model=svm_model,
